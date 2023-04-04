4106a724114c808cfc22f52e15b31c477f6b33af
Alexandr Usnarski <>
2023-04-04 14:46:42 +0300

    Create project.

---
+++ FoodProject/FoodProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodProject
+//
+//  Created by Alexandr Usnarski on 4.04.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
